d97f55f48ec5e16e7a5fd9c3e03a346380b26833
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-02-12 19:05:37 +0700

    onCLick in button and live Data

--- MVVM/01_DataBinding/app/src/main/java/com/rizalfadiaalfikri/databinding/MainActivity.java
+++ MVVM/01_DataBinding/app/src/main/java/com/rizalfadiaalfikri/databinding/MainActivity.java
@@ -2,6 +2,8 @@ package com.rizalfadiaalfikri.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
@@ -10,12 +12,17 @@ import com.rizalfadiaalfikri.databinding.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        Mahasiswa mahasiswa = new Mahasiswa("Rizal Fadia Al Fikri", "19402092");
-        binding.setMahasiswa(mahasiswa);
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+//        Mahasiswa mahasiswa = new Mahasiswa("Rizal Fadia Al Fikri", "19402092");
+//        binding.setMahasiswa(mahasiswa);
+        binding.setViewModel(viewModel);
+        binding.setLifecycleOwner(this);
     }
+
 }
--- MVVM/01_DataBinding/app/src/main/java/com/rizalfadiaalfikri/databinding/MainActivityViewModel.java
+++ MVVM/01_DataBinding/app/src/main/java/com/rizalfadiaalfikri/databinding/MainActivityViewModel.java
@@ -1,6 +1,18 @@
 package com.rizalfadiaalfikri.databinding;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainActivityViewModel extends ViewModel {
+    private MutableLiveData<Mahasiswa> mahasiswaMutableLiveData = new MutableLiveData<>();
+
+    public void updateNamaMahasiswa() {
+        Mahasiswa mahasiswa = new Mahasiswa("Cid Kagenou", "19402091");
+        mahasiswaMutableLiveData.setValue(mahasiswa);
+    }
+
+    public LiveData<Mahasiswa> getMahasiswaLiveData() {
+        return mahasiswaMutableLiveData;
+    }
 }
