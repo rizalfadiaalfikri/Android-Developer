b49f751134dcde9d9f9cb8867ffeea9693913490
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-02-12 14:53:20 +0700

    adding exspression layout for activityMain.xml

--- MVVM/01_DataBinding/app/src/main/java/com/rizalfadiaalfikri/databinding/MainActivity.java
+++ MVVM/01_DataBinding/app/src/main/java/com/rizalfadiaalfikri/databinding/MainActivity.java
@@ -1,14 +1,21 @@
 package com.rizalfadiaalfikri.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.rizalfadiaalfikri.databinding.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Mahasiswa mahasiswa = new Mahasiswa("Rizal Fadia Al Fikri", "19402092");
+        binding.setMahasiswa(mahasiswa);
     }
 }
