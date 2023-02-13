439fff1bda20331414cb42a6e2c7fdabdbfef8e8
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2023-02-13 20:52:06 +0700

    add dependency and setup data binding for 04_NavigationComponent

--- MVVM/04_NavigationComponent/app/src/main/java/com/rizalfadiaalfikri/navigationcomponent/MainActivity.java
+++ MVVM/04_NavigationComponent/app/src/main/java/com/rizalfadiaalfikri/navigationcomponent/MainActivity.java
@@ -1,14 +1,17 @@
 package com.rizalfadiaalfikri.navigationcomponent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.rizalfadiaalfikri.navigationcomponent.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
